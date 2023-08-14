1e0cdeb218680c1b253ff1b653c6b98d581fdf98
anushukla1808 <38080827+anushukla1808@users.noreply.github.com>
2023-08-15 00:32:36 +0100

    Update IMDb.py
    The model was training itself on corrupted data. Now, for every corruption ratio, dataset will be reset to its original form and run_experiment() will be called.

--- IMDb.py
+++ IMDb.py
@@ -9,27 +9,24 @@ from keras.callbacks import EarlyStopping
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import numpy as np
 
-#Set the random seed for reproducibility
+# Set the random seed for reproducibility
 np.random.seed(7)
 
-# Load the dataset
+# Load and pad the dataset once
 top_words = 5000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+(X_original, y_original), (X_test, y_test) = imdb.load_data(num_words=top_words)
 
-# Truncate and pad input sequences
 max_review_length = 500
-X_train = pad_sequences(X_train, maxlen=max_review_length)  
-X_test = pad_sequences(X_test, maxlen=max_review_length)  
 
-# Specify the embedding vector length
+X_original_padded = pad_sequences(X_original, maxlen=max_review_length)
+X_test_padded = pad_sequences(X_test, maxlen=max_review_length)
 embedding_vector_length = 32
 
-def run_experiment(X_train, y_train, X_test, y_test, corruption_ratio):
-    # Corrupt the labels
-    num_corrupted = int(corruption_ratio * len(y_train))
-    corrupted_indices = np.random.choice(len(y_train), size=num_corrupted, replace=False)
-    y_train_corrupted = y_train.copy()
-    y_train_corrupted[corrupted_indices] = 1 - y_train[corrupted_indices]
+def run_experiment(X_train, y_train, X_test, y_test, corruption_ratio=0.0):
+    if corruption_ratio:
+        num_corrupted = int(corruption_ratio * len(y_train))
+        corrupted_indices = np.random.choice(len(y_train), size=num_corrupted, replace=False)
+        y_train[corrupted_indices] = 1 - y_train[corrupted_indices]
 
     # Build the model
     model = Sequential()
@@ -40,21 +37,21 @@ def run_experiment(X_train, y_train, X_test, y_test, corruption_ratio):
 
     # Train the model
     earlystop = EarlyStopping(monitor='val_loss', patience=3)
-    model.fit(X_train, y_train_corrupted, validation_data=(X_test, y_test), epochs=10, batch_size=64, callbacks=[earlystop])
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=64, callbacks=[earlystop])
 
     # Evaluate the model
     y_pred_prob = model.predict(X_test)
-    y_pred = np.where(y_pred_prob > 0.5, 1, 0)  # convert probabilities to class labels
+    y_pred = np.where(y_pred_prob > 0.5, 1, 0)
     accuracy = accuracy_score(y_test, y_pred)
     precision, recall, f1_score, _ = precision_recall_fscore_support(y_test, y_pred, average='binary')
 
     return accuracy, precision, recall, f1_score
 
-# Define the corruption ratios
-ratios = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
+# Evaluate on clean data
+accuracy, precision, recall, f1_score = run_experiment(X_original_padded, y_original.copy(), X_test_padded, y_test)
+print(f"Corruption Ratio: 0, Test Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-score: {f1_score}")
 
-# Run the experiment for each corruption ratio
-for r in ratios:
-    accuracy, precision, recall, f1_score = run_experiment(X_train, y_train, X_test, y_test, r)
+# For each corruption ratio
+for r in [0.1, 0.3, 0.5, 0.7, 0.9]:
+    accuracy, precision, recall, f1_score = run_experiment(X_original_padded.copy(), y_original.copy(), X_test_padded, y_test, r)
     print(f"Corruption Ratio: {r}, Test Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-score: {f1_score}")
-
